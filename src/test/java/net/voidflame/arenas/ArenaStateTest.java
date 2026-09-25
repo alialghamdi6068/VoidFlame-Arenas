7305bc1cd469c4291833c73b9766d3294607fb47
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 18:47:59 +0300

    Strengthen arena lifecycle test

--- src/test/java/net/voidflame/arenas/ArenaStateTest.java
+++ src/test/java/net/voidflame/arenas/ArenaStateTest.java
@@ -2,11 +2,11 @@ package net.voidflame.arenas;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArenaStateTest {
     @Test
-    void lifecycleStatesAreDefined() {
+    void exposesTheCompleteLifecycle() {
         assertEquals(ArenaState.AVAILABLE, ArenaState.valueOf("AVAILABLE"));
         assertEquals(ArenaState.IN_USE, ArenaState.valueOf("IN_USE"));
         assertEquals(ArenaState.DISABLED, ArenaState.valueOf("DISABLED"));
